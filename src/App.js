05fa5cf9b02c4b28d5743eb3e5b64a0f9b4d49a4
Satyam Singh <satyamsingh20027@gmail.com>
2021-11-13 09:59:45 +0530

    Tailwind Added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,19 @@
-import "./index.css";
-import { useState, useEffect } from "react";
-import { supabase } from "./supabase/client";
-import Auth from "./Auth";
-import Account from "./Account";
+import './index.css'
 
-export default function App() {
-    const [session, setSession] = useState(null);
-
-    useEffect(() => {
-        setSession(supabase.auth.session());
-
-        supabase.auth.onAuthStateChange((_event, session) => {
-            setSession(session);
-        });
-    }, []);
-
+function App() {
     return (
-        <div className="container" style={{ padding: "50px 0 100px 0" }}>
-            {!session ? (
-                <Auth />
-            ) : (
-                <Account key={session.user.id} session={session} />
-            )}
+        <div className="bg-gray-900 p-20 h-screen flex justify-center items-start flex-col">
+            <h1 className="text-5xl text-white">Hello Tailwind 👋</h1>
+            <p className="text-gray-400 mt-5 text-lg">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit
+                consequuntur odio aut nobis ab quis? Reiciendis doloremque ut quo fugiat
+                eveniet tempora, atque alias earum ullam inventore itaque sapiente iste?
+            </p>
+            <button class="p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600">
+                Hello Friends 🚀
+            </button>
         </div>
-    );
+    )
 }
+
+export default App
