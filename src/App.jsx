d6548622d125349464705020dd573158a9900406
Aaryan-coder <76030621+Aaryan-coder@users.noreply.github.com>
2021-11-17 23:56:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Register from './forms/Register';
 import Home from './content/Home';
 import Events from './content/events/Events';
 import IndividualEvent from './content/events/IndivualEvent';
+import Footer from './footer/Footer';
 
 const App = () => (
   <Router>
@@ -15,8 +16,9 @@ const App = () => (
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/events" element={<Events />} />
-      <Route path="/events/:eventId/:eventName" element={<IndividualEvent />} />
+      <Route path="/events/:eventId" element={<IndividualEvent />} />
     </Routes>
+    <Footer />
   </Router>
 );
 
