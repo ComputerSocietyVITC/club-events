84c076d977c22bb3769d3ff6941ae7f3c8470c00
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-11-19 21:51:11 +0530

    Addition of individual events component

--- src/content/events/IndivualEvent.jsx
+++ src/content/events/IndivualEvent.jsx
@@ -1,12 +1,159 @@
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 
 const IndividualEvent = () => {
-  console.log('this is working');
-  const { eventId, eventName } = useParams();
+  // const { eventId, eventName } = useParams();
   return (
-    <p className="text-white">
-      This is event id {eventId} and it's called {eventName}
-    </p>
+    <>
+      <section className="grid grid-rows-12 gap-3">
+        <section className="row-start-1 row-span-4">
+          <section className="text-white font-monty text-center">
+            <section className="pl-6 py-4 grid grid-cols-6 sm:grid-cols-6 md:grid-cols-6">
+              <section className="px-200 col-start-1 rounded-full h-24 w-24 bg-cyan " />
+              <section className="col-start-2 font-monty text-white text-left">
+                <section className="grid grid-rows-10 gap-4">
+                  <section className="row-start-1 row-span-2 Indievents-title">[Title]</section>
+                  <section className="row-start-3 Indievents-subtitle">[SubTitle]</section>
+                  <section className="pl-2 pr-2  py-2 row-start-4 row-span-1 text-center rounded-large bg-cyan text-black w-max">
+                    Location: [Platform] | Registration-Deadline: [Data]
+                  </section>
+                  <section className="row-start-6 row-span-2 py-2 pl-4 pr-4 rounded-full bg-cyan text-black text-center w-max">
+                    Team Size: [Member Count] Members
+                  </section>
+                </section>
+                <br />
+              </section>
+              <section className="col-start-5">
+                <section className="grid grid-rows gap-3">
+                  <section className="row-start-1">
+                    <section className="grid grid-cols-4 gap-2">
+                      <section className="py-3 col-start-1 rounded-large text-center col-span-3 bg-white text-black font-monty font-bold">
+                        EDIT EVENTS
+                      </section>
+                      <section className="col-start-4">
+                        <section className="rounded-full h-12 w-12 bg-footercolor" />
+                      </section>
+                    </section>
+                  </section>
+                  <section className="row-start-2">
+                    <section className="px-4 rounded-full bg-cyan text-black w-max">
+                      [Start-Date to End-date]
+                    </section>
+                  </section>
+                  <section className="px-3 row-start-3 text-black bg-footercolor rounded-large">
+                    [time] | [members]
+                    <center>
+                      <section className="px-3 py-2 pb-2 rounded-large bg-white text-black w-max">
+                        Check Analytics
+                      </section>
+                      <br />
+                    </center>
+                  </section>
+                </section>
+              </section>
+            </section>
+          </section>
+        </section>
+
+        <section className="row-start-5 row-span-5">
+          <section className="px-4 grid grid-cols-12">
+            <section className="col-start-2 col-span-10">
+              <section className="py-4 row-start-9 row-span-1 text-white font-monty items-center rounded-large w-full h-min bg-darkergreen">
+                <p className="pl-4 pr-4">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam nec erat ut mi
+                  pretium condimentum sed id sapien. Sed id laoreet felis, vel pretium mauris.
+                  Vestibulum orci dolor, commodo eu sem at, scelerisque tincidunt nisl. Ut tincidunt
+                  eros et tortor fringilla, eu facilisis leo tincidunt. Sed risus tellus, pharetra
+                  sit amet iaculis vitae, vulputate sed erat. Pellentesque quam magna, finibus eget
+                  tristique a, porta a leo. Donec augue augue, euismod eget magna sit amet, euismod
+                  ornare sapien. Ut vitae massa faucibus, lobortis nisl quis, molestie velit. Sed
+                  faucibus, ipsum et ornare tristique, nibh leo interdum elit, ac fermentum velit
+                  ipsum vel orci. Cras non arcu fermentum, molestie libero vel, pellentesque erat.
+                  Duis efficitur volutpat neque nec cursus. Duis sit amet lorem sit amet orci
+                  condimentum elementum sed rutrum quam. Interdum et malesuada fames ac ante ipsum
+                  primis in faucibus. Proin eu urna a erat ultrices maximus. Donec ut mi pulvinar,
+                  varius sem nec, commodo neque. Donec non congue diam. Integer et ligula
+                  vestibulum, fermentum metus vehicula, ultricies metus. Cras a ante finibus arcu
+                  interdum malesuada. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                  Mauris pharetra eu orci maximus pretium. Vestibulum cursus ultricies velit ut
+                  scelerisque. Suspendisse potenti. Quisque imperdiet ornare magna, vitae luctus
+                  metus rhoncus nec. Maecenas eleifend egestas iaculis. Donec molestie lorem id ex
+                  aliquam, vel cursus odio viverra. Suspendisse sodales facilisis sapien ac
+                  vestibulum. Suspendisse id velit scelerisque, tincidunt ex eu, interdum metus.
+                  Integer dictum elit nec cursus posuere. Pellentesque quis magna rutrum, tristique
+                  leo eget, pretium nunc. Nunc at feugiat lorem. Vivamus vitae neque mattis,
+                  fermentum tellus et, ullamcorper mi. Curabitur a tincidunt felis. Pellentesque at
+                  metus quis lacus tempor ultricies eget sit amet neque. Etiam sodales leo velit, at
+                  commodo mauris facilisis at. Nullam porta sem sit amet sodales scelerisque. Nunc
+                  non massa sem. Cras eu augue non nisl molestie bibendum. Interdum et malesuada
+                  fames ac ante ipsum primis in faucibus. Nulla aliquam faucibus nisl vitae
+                  sollicitudin. Phasellus convallis quis lectus sit amet auctor. Suspendisse pretium
+                  justo sed felis posuere volutpat. Maecenas et metus cursus, molestie sapien vel,
+                  malesuada urna. Phasellus velit arcu, tristique sed rhoncus vel, porta vel lectus.
+                  Pellentesque condimentum massa eu eros venenatis, varius varius diam lacinia.
+                  Praesent eu lorem ornare lorem tincidunt sagittis quis feugiat tortor. Maecenas
+                  accumsan orci elit, et venenatis elit vehicula id. Suspendisse eget mauris eu
+                  magna euismod tincidunt. Sed vel libero ullamcorper, facilisis lorem sed, porta
+                  tortor. Phasellus volutpat enim sit amet nisl euismod vehicula. Sed ut mattis
+                  arcu. Donec tincidunt ante quis lectus ultrices cursus vitae eget tellus. Nulla
+                  facilisi. Aenean placerat quam ligula, ullamcorper feugiat tellus auctor ut. Donec
+                  vehicula, est a sollicitudin scelerisque, arcu nisl ornare felis, id volutpat nisi
+                  nulla ac velit. Aenean finibus risus at arcu lobortis, eu efficitur nunc vehicula.
+                  In euismod tellus ultricies, consequat magna at, consequat lorem. Aliquam
+                  efficitur ex molestie sapien luctus lacinia. Nullam ac arcu venenatis, mattis orci
+                  nec, lobortis nisl. Proin aliquet erat sed mi tempor consequat. Duis blandit,
+                  metus ac semper viverra, orci libero efficitur nisl, sit amet rhoncus augue lacus
+                  luctus dolor. Aenean rhoncus quam vel porta posuere. Proin in volutpat lorem. In a
+                  urna mi. Duis vel orci vitae tortor vestibulum accumsan. Nunc quis eros tristique
+                  libero ornare interdum. Integer sit amet ligula in dolor vulputate suscipit
+                  bibendum ut eros. Morbi mattis id leo quis dictum.
+                </p>
+              </section>
+            </section>
+          </section>
+        </section>
+
+        <section className="row-start-12">
+          <section className="px-4 grid grid-cols-12">
+            <section className="px-4 py-2 col-start-2 col-span-10  bg-cyan rounded-large">
+              <section className="py-4 font-monty font-bold text-center text-black items-center text-4xl">
+                Held By
+                <center>
+                  <svg
+                    width="424"
+                    height="3"
+                    viewBox="0 0 424 3"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <line
+                      x1="0.498047"
+                      y1="1.25"
+                      x2="423.753"
+                      y2="1.25"
+                      stroke="#222222"
+                      stroke-width="2"
+                    />
+                  </svg>
+                </center>
+              </section>
+              <section className="font-monty text-2xl text-center">[Club]</section>
+              <section className="font-monty text-l text-left">
+                Ut vitae massa faucibus, lobortis nisl quis, molestie velit. Sed faucibus, ipsum et
+                ornare tristique, nibh leo interdum elit, ac fermentum velit ipsum vel orci. Cras
+                non arcu fermentum, molestie libero vel, pellentesque erat. Duis efficitur volutpat
+                neque nec cursus. Duis sit amet lorem sit amet orci condimentum elementum sed rutrum
+                quam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Proin eu urna a
+                erat ultrices maximus. Donec ut mi pulvinar, varius sem nec, commodo neque. Donec
+                non congue diam. Integer et ligula vestibulum, fermentum metus vehicula, ultricies
+                metus. Cras a ante finibus arcu interdum malesuada.
+              </section>
+            </section>
+          </section>
+        </section>
+
+        <br />
+      </section>
+    </>
   );
 };
 
