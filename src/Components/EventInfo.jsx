dbc0a0f7aeb558c91c3fae018e444edf3644e3d8
Satyam Singh <satyamsingh20027@gmail.com>
2021-11-21 21:09:02 +0530

    responsiveness improved

--- src/Components/EventInfo.jsx
+++ src/Components/EventInfo.jsx
@@ -2,18 +2,10 @@ import React from 'react'
 
 function EventInfo() {
     return (
-        <div className='mt-3  text-xs  text-white  max-w-9/10 md:text-base ml-5 mmd:text-xl mmd:mt-4 mmd:pt-8 pb-8'>
+        <div className='mt-3  text-sm  text-white pb-8   ml-5 sm:text-xl md:mt-4 md:pt-8 md:text-2xl lg:text-4xl '>
+
             <p>club events info to be added here here</p>
             <p>club events info to be added here here</p>
-            <p>club events info to be added here here</p>
-            <p>club events info to be added here here</p>
-
-
-
-
-
-
-
 
 
 
--- src/content/Home.jsx
+++ src/content/Home.jsx
@@ -8,29 +8,24 @@ import Button from '../Components/Button';
 
 const Home = () => {
   return (
-    <div className='w-full flex flex-row h-full'>
-
-      <div className='flex flex-col   flex-1 '>
-        <div className='absolute mmd:mt-4  '>
-
-          <h1 className='md:text-3xl px-6 mt-8  font-bold text-white  mmd:mt-12 mmd:ml-8 mmd:text-5xl mlg:text-6xl'>VIT CHENNAI</h1>
-          <h1 className='md:text-3xl px-6 font-bold text-white  mmd:mt-2  mmd:ml-8   mmd:text-5xl mlg:text-6xl '>CLUB EVENTS!</h1>
-
+    <div className='w-100vh flex flex-1 flex-col h-full overflow-hidden'>
+      <div className='flex flex-col justify-start'>
+        <Blob1 className='h-40 w-48 sm:h-48 sm:w-56 md:h-72 md:w-80 lg:h-sm lg:w-md' />
+        <div className='ml-8 mt-8 text-4xl absolute text-white font-bold sm:text-5xl md:text-6xl md:mt-16 lg:mt-28 lg:ml-24 lg:text-8xl '>
+          <h1>Vit Chennai</h1>
+          <h1>Club Events</h1>
         </div>
-        <Blob1 className='md:h-40 md:w-48 mmd:h-96 mmd:w-96 mmd:-mt-12 mlg:h-96 mlg:w-1/2 mlg:mt-24' />
         <EventInfo />
+
+
         <Button />
       </div>
-
-      <div className='flex-row mmd:flex-1 mt-12  overflow-hidden '>
-
-
-        <Illustration className=' xs:w-32 xs:h-64 ml-0 md:h-64 md:w-64 mmd:h-96 w-sm mlg:absolute mlg:ml-16 mlg:w-sm' />
-
-        <Blob2 className=' lg:h-0 lg:w-0 h-sm w-auto ml:64 mxl:ml-80  overflow-hidden  mlg:mt-48' />
-
+      <div />
+      <div className='flex justify-end  xl:-mt-40'>
+        <Illustration className='h-64 w-48 absolute  -mt-28 sm:w-64 md:h-72 md:w-80  md:-mt-36 lg:h-sm lg:w-md lg:-mt-64' />
+        <Blob2 className='h-40 w-52  sm:h-40 sm:w-48 md:h-64 md:w-72 lg:h-md lg:w-lg' />
       </div>
-    </div>
+    </div >
 
 
 
