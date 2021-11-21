d0b0b4292cfcf4ec06434c6d068e0d49902840a1
Satyam Singh <satyamsingh20027@gmail.com>
2021-11-21 21:14:02 +0530

    Tailwind config changed
    Removed the screens config so as to follow tailwinds mobile first approach.

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button() {
     return (
-        <button className="ml-6 mt-2 h-8 mb-4 w-32 bg-lightergreen hover:bg-green-500 text-black font-bold   text-sm border border-blue-700 rounded md:w-60 md:h-16 md:text-2xl lg:mt-12 lg:h-20 lg:w-72">
+        <button className="ml-6 mt-2 h-8 mb-4 w-32 bg-lightergreen hover:bg-green-500 text-black font-bold   text-sm border border-blue-700 rounded md:w-60 md:h-16 md:text-2xl lg:h-20 lg:w-72">
             Let's Go!
         </button>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -29,43 +29,7 @@ module.exports = {
       fontFamily: {
         normal: ['Arial'],
       },
-      screens: {
-        '2xl': { max: '1535px' },
-        // => @media (max-width: 1535px) { ... }
 
-        xl: { max: '1279px' },
-        // => @media (max-width: 1279px) { ... }
-
-        lg: { max: '1023px' },
-        // => @media (max-width: 1023px) { ... }
-
-        md: { max: '767px' },
-        // => @media (max-width: 767px) { ... }
-
-        sm: { max: '639px' },
-        // => @media (max-width: 639px) { ... }
-
-        xs: { max: '359px' },
-        // => @media (max-width: 359px) { ... }
-        '2mxl': { min: '1535px' },
-        // => @media (max-width: 1535px) { ... }
-
-        mxl: { min: '1279px' },
-        // => @media (max-width: 1279px) { ... }
-
-        mlg: { min: '1023px' },
-        // => @media (max-width: 1023px) { ... }
-
-        mmd: { min: '767px' },
-        // => @media (max-width: 767px) { ... }
-
-        msm: { min: '639px' },
-        // => @media (max-width: 639px) { ... }
-
-        mxs: { min: '359px' },
-        // => @media (max-width: 359px) { ... }
-
-      },
       gridTemplateColumns: {
         '2to1': '2fr 1fr',
       },
