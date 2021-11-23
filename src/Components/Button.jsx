37fcae1e585afd6dff30464d4a85fa97e16c97c3
Aaryan-coder <76030621+Aaryan-coder@users.noreply.github.com>
2021-11-23 08:22:55 +0530

    Update Button.jsx
    Adding link for dashboard

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom';
 function Button() {
     return (
         <button className="mt-12 mb-8 ml-4 py-2 max-w-xs bg-lightergreen hover:bg-green-500 text-black font-bold   text-sm border border-blue-700 rounded-full ">
-            Let's Go!
+        <Link to="events/Dashboard">Let's Go!</Link>
         </button>
     )
 }
