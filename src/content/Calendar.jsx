50494bf728f97934ff1f192615941e85658d39cf
Samank Gupta <samankgupta@gmail.com>
2021-12-01 14:34:53 +0530

    replaced moment.js with date-fns

--- src/content/Calendar.jsx
+++ src/content/Calendar.jsx
@@ -1,18 +1,31 @@
-import { Calendar, momentLocalizer } from 'react-big-calendar'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
 import "react-big-calendar/lib/css/react-big-calendar.css"
-import moment from 'moment'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import enUS from 'date-fns/locale/en-US'
 import '../Assets/Calendar.css'
 
 const CalendarPage = () => {
 
-    const localizer = momentLocalizer(moment)
+    const locales = {
+        'en-US': enUS,
+    }
+
+    const localizer = dateFnsLocalizer({
+        format,
+        parse,
+        startOfWeek,
+        getDay,
+        locales,
+    })
+
     const state = {
         events: [
             {
-                start: moment().toDate(),
-                end: moment()
-                    .add(2, "days")
-                    .toDate(),
+                start: new Date(2021, 11, 11),
+                end: new Date(2021, 11, 13),
                 title: "Temp Event"
             }
         ]
@@ -22,8 +35,8 @@ const CalendarPage = () => {
         <div className="m-20">
             <Calendar
                 localizer={localizer}
-                defaultDate={new Date()}
-                defaultView="month"
+                startAccessor="start"
+                endAccessor="end"
                 events={state.events}
                 style={{ height: "100vh" }}
             />
