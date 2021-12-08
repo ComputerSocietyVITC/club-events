ec040e90dd4beebbbc1c9a7522b67a7ea8a82544
Abhijith Ganesh <g.abhijith14122003@gmail.com>
2021-12-08 08:08:47 +0530

    Addition of analytics and new styling of create event

--- src/content/events/analytics.jsx
+++ src/content/events/analytics.jsx
@@ -3,103 +3,63 @@ import { BiArrowBack } from 'react-icons/bi';
 import { BsFillPlusCircleFill, BsToggle2Off, BsToggle2On } from 'react-icons/bs';
 import { CgAttachment } from 'react-icons/cg';
 
+
+const InnerBox = ({Name}) => {
+  return(
+    <>
+      <section className='my-2 mx-4 py-4 px-4 bg-innerboxgreen text-white font-monty'>
+        <section className='grid grid-cols-8'>
+          <section className='col-start-1'>
+            <img style = {{
+              height:60,
+              width:60,
+              borderRadius:60
+            }}src="https://png.pngitem.com/pimgs/s/105-1050694_user-placeholder-image-png-transparent-png.png"/>
+          </section>
+          <section className='center col-start-2 col-span-3'>
+            {Name}
+          </section>
+        </section>
+      </section>
+    </>
+  )
+}
+
 const EventAnalytics = () => {
   const Iconstyles = {
     height: '50px',
-    width:"50px"
+    width: '50px',
   };
   const [toggle, setToggle] = useState(true);
   return (
-    <section className="grid xl:grid-cols-12 lg:grid-cols-12 sm:grid-cols-6">
-      <section className="pl-10 pr-2 py-3 col-start-1">
-        <BiArrowBack className="text-white text-3xl" />
-      </section>
-      <section className="py-2 sm:col-start-2 sm:col-span-4 lg:col-start-2 lg:col-span-8 xl:col-start-2 xl:col-span-8">
-        <section className="text-white text-4xl font-monty font-extrabold">Create Event</section>
-        <section className="py-4 text-white text-xl font-monty font-semibold">Event Name</section>
-        <section>
-          <input className="bg-boxgreen w-full rounded-large" type="text" />
+    <>
+      <section className=" py-2 grid grid-cols-8 xl:grid-cols-8 lg:grid-cols-6 md:grid-cols-4">
+        <section className="pl-10 text-white text-3xl col-start-1 col-span-1">
+          <BiArrowBack />
         </section>
-        <section className="flex flex-row py-3 text-white text-xl font-monty font-semibold">
-          <section className=" px-2">
-            Event Timings <input className="bg-boxgreen w-full rounded-large" type="time" />
+        <section className="col-start-2 col-span-6 gap-2">
+          <section className="py-2 text-white font-monty font-bold text-3xl">
+            Event Analytics
           </section>
-          <section className=" px-2">
-            Event Registration
-            <input className="bg-boxgreen w-full rounded-large" type="calendar" />
+          <section className="py-2 text-white font-monty font-bold text-2xl">
+            Time-Left : [TIME]
           </section>
-        </section>
-        <section>
-          <section className="flex flex-row py-3 text-white text-xl font-monty font-semibold">
-            <section className=" px-2">
-              Categories <input className="bg-boxgreen w-full rounded-large" type="text" />
+          <section className="py-2 text-white font-monty font-bold text-2xl">
+            Hosted By
+            <section className="mt-2 px-3 rounded-large bg-boxgreen w-full">
+              IEEE Computer Society
             </section>
-            <section className=" px-2">
-              Team size
-              <input className="bg-boxgreen w-full rounded-large" type="text" />
-            </section>
-          </section>
-          <section>
-            <section className="font-monty h-full text-white text-xl font-semibold">Event Info</section>
-            <section className="bg-boxgreen  rounded-large">
-              <section className="text-white font-monty text-lg px-0">
-                <textarea name="Event Info" cols="100" rows="10" className="pl-3 bg-boxgreen w-full rounded-large max-h-full h-32"></textarea>
-              </section>
-            </section>
-          </section>
-          <section className="text-white text-lg font-monty font-semibold">
-            Do you want users to register to this event from our portal?
-            <section className="transition duration-1000">
-              {(() => {
-                if (toggle) {
-                  return (
-                    <>
-                      <BsToggle2Off style={Iconstyles} onClick = {
-                        () => {
-                          setToggle(!toggle)
-                        }
-                      }/>
-                    </>
-                  );
-                } else {
-                  return (
-                    <>
-                      <BsToggle2On style={Iconstyles} onClick = {
-                        () => {
-                          setToggle(!toggle)
-                        }}/>
-                    </>
-                  );
-                }
-              })()}
-            </section>
-          </section>
-          <section className="flex flex-box py-3 justify-start">
-            <section className="text-white px-2 font-monty font-semibold text-lg">
-              Co-ordinator Name <input className="bg-boxgreen w-full rounded-large" type="text" />
-            </section>
-            <section className="text-white pr-2 font-monty font-semibold text-lg">
-              Contact Name
-              <input className="bg-boxgreen w-full rounded-large" type="text" />
-            </section>
-          </section>
-          <section className="text-white font-monty text-lg">Add Attachments</section>
-        </section>
-      </section>
-      <section className="col-start-2 col-span-3 text-white px-1 py-2 text-3xl">
-        <section className="hover:transform-gpu hover: transition translate-x-1 flex flex-box justify-start gap-2">
-          <section>
-            <BsFillPlusCircleFill/>
-          </section>
-          <section className="bg-boxgreen px-2 text-white font-monty text-xl rounded-large">
-            <section className="flex flex-box justify-evenly">
-              <CgAttachment style={{ paddingTop: '8px', color: 'white' }} />
-              Add Attachments
+            <section className="py-3 text-white font-monty font-bold">Registered Users</section>
+            <section className="py-4 bg-boxgreen rounded-large px-2 h-full">
+              <InnerBox Name = "Participant 1"/>
+              <InnerBox Name = "Participant 2"/>
+              <InnerBox Name = "Participant 3"/>
+              <InnerBox Name = "Participant 4"/>
             </section>
           </section>
         </section>
       </section>
-    </section>
+    </>
   );
 };
 
